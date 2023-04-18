91fac08f97be973226ceff6054b6e014c89ffa19
Nilufar Najiyeva <127186140+nilufarnajiyeva@users.noreply.github.com>
2023-04-18 11:25:39 +0400

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,35 @@
+// function areaofcircle(radius){
+//     return 3*radius*radius
+// }
+// var a =areaofcircle(prompt())
+// console.log(a)
+
+
+
+
+
+//  function areaofsquare(a,b){
+//     var a,b;
+//     return a*b
+//  }
+//  var d=areaofsquare(7,8)
+//  console.log(d)
+
+
+
+// function parsahe(a,b,d){
+//     var a,b,d;
+//     return 2*(a*b+a*d+b*d)
+// }
+// var h=parsahe(4,6,3)
+// console.log(h)
+
+
+
+// function ucbucaq(a,c,b,r){
+//     var a,b,c,r
+//     var  p=(a+b+c)/2
+// return p*r
+// }
+// var j=ucbucaq(2,4,6,8)
+// console.log(j)
